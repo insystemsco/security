e9212e53fad63af56b5cc29dc1d0b29236008c1b
n0mad <47467562+insystemsco@users.noreply.github.com>
2019-02-14 12:24:15 -0500

    update incident response
    corrected some errors and added a few new functions.

--- lr.bat
+++ lr.bat
@@ -1,15 +1,22 @@
-:: TR3 - rewrite to be effective on newer windows versions. And use
+:: TR3 - re-write to be effective on newer windows versions.
+:: Incident Response Collection
+:: converting what I can from invoke-ir powershell to batch...
+:: http://www.invoke-ir.com/
+::
+:: Jim Price
+:: 2/9/19
+::
 ::	Script start...
 @echo off
 ::	Change Directory to the script's one, since Stupid windows will execute at "\windows\system32" when chosen to run as administrator...
 cd /d "%~d0%~p0"
 
 ::Get Date and Time
-For /F "tokens=1,2,3,4 delims=/ " %%A in ('Date /t') do @( 
+For /F "tokens=1,2,3,4 delims=/ " %%A in ('Date /t') do @(
 Set FullDate="%%D-%%C-%%B"
 )
 
-For /F "tokens=1,2,3 delims=: " %%A in ('time /t') do @( 
+For /F "tokens=1,2,3 delims=: " %%A in ('time /t') do @(
 Set FullTime="%%A-%%B"
 )
 
@@ -32,26 +39,30 @@ echo ======================================== >> %var%
 cmd /c "%%a" >> %var%
 )
 
-::extract tools
+::extract tools - tools used in tools.txt
 "c:\program files (x86)"\tanium\tanium clienttools\StdUtils\7za.exe e "c:\program files (x86)\tanium\tanium clienttools\tr3\deps.zip"  -o"\%dirname%\" > %dirname%\deps.log
-
+::
 cd ..
 cd Tools
 autorunsc.exe -a -f -c >> ..\%dirname%\autoruns.csv
 handle.exe -a -u >> ..\%dirname%\handles.txt
 HiJackThis.EXE /silentautolog
 move hijackthis.log ..\%dirname%\
-listdlls.exe >> ..\%dirname%\dlls.txt
-psinfo -s -h -d >> ..\%dirname%\psinfo.txt
-pslist -t >> ..\%dirname%\pslist.txt
-psgetsid >> ..\%dirname%\psgetsid.txt
-psloggedon.exe >> ..\%dirname%\psloggedon.txt
-Tcpvcon.exe -a -c -n >> ..\%dirname%\tcpvcon.txt
-handle.exe >> ..\%dirname%\handle.txt
+listdlls.exe -accepteula >> ..\%dirname%\dlls.txt
+psinfo -accepteula -s -h -d >> ..\%dirname%\psinfo.txt
+pslist -accepteula -t >> ..\%dirname%\pslist.txt
+psgetsid accepteula >> ..\%dirname%\psgetsid.txt
+psloggedon.exe -accepteula >> ..\%dirname%\psloggedon.txt
+Tcpvcon.exe -accepteula -a -c -n >> ..\%dirname%\tcpvcon.txt
+psfile.exe -accepteula >> ..\%dirname%\openfiles.txt
+psloglist.exe -accepteula -g %computername% >> ..\%dirname\
+procdump.exe -accepteula -ma >> ..\%dirname%\processes
+streams.exe -accepteula -s c:\ >> ..\%dirname%\streams.txt
+
 
 ::Get system info
 @echo off
-echo +++++Live Response Collection+++++ >> %computername%.txt
+echo +++++Incident Response Collection+++++ >> %computername%.txt
 echo +++++Date and Time+++++ >> %computername%.txt
 date /t >> %computername%.txt
 time /t >> %computername%.txt
@@ -71,3 +82,4 @@ echo +++++DNS Cache+++++ >> %computername%.txt
 ipconfig /displaydns >> %computername%.txt
 echo +++++Network Connections+++++ >> %computername%.txt
 netstat -abn >> %computername%.txt
+netstat -ano >> %computername%.txt
